92975a08901b9f9a563ac070db5f17fab401764c
schwingi2 <albert.schwingenschloegl@gmail.com>
2023-12-28 04:45:53 -0500

    Week2 Basic login logout and registration

--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -32,11 +32,15 @@ def login_request(request):
 # ...
 
 # Create a `logout_request` view to handle sign out request
-# def logout_request(request):
+def logout_request(request):
+    logout(request)
+    return redirect('djangoapp:index')
 # ...
 
+
 # Create a `registration_request` view to handle sign up request
-# def registration_request(request):
+def registration_request(request):
+    path('registration/', views.registration_request, name='registration')
 # ...
 
 # Update the `get_dealerships` view to render the index page with a list of dealerships
