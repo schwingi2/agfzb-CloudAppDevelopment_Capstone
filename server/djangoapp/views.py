4a26578d8a048e0d2867cf931c60573411e7fe8e
schwingi2 <albert.schwingenschloegl@gmail.com>
2023-12-29 06:16:59 -0500

    Login and Logout but without working signup functionality

--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -28,13 +28,27 @@ def contact(request):
 
 # Create a `login_request` view to handle sign in request
 def login_request(request):
-    return render(request,'djangoapp/login.html')
+    context = {}
+    if request.method == "POST":
+        username = request.POST['username']
+        password = request.POST['psw']
+        user = authenticate(username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect('djangoapp:index')
+        else:
+            context['message'] = "Invalid username or password."
+            return render(request, 'djangoapp/login.html', context)
+    else:
+        return render(request, 'djangoapp/login.html', context)
 # ...
 
 # Create a `logout_request` view to handle sign out request
 def logout_request(request):
+    print("Log out the user `{}`".format(request.user.username))
     logout(request)
     return redirect('djangoapp:index')
+
 # ...
 
 
